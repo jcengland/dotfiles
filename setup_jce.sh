754f37c306a0e9b98896dbe25b4cf8d40db8f2a7
Clay England <clay.england@gmail.com>
2021-01-12 20:02:50 -0500

    added checks for apps

--- setup_jce.sh
+++ setup_jce.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
+which git > /dev/null
+GIT=$?
+
+which curl > /dev/null
+CURL=$?
+
+
 #############################
 #  Get tmux plugin manager  #
 #############################
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [[ ${GIT} -eq 0 ]]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+elif
+echo "Git not found"
+fi
 
 
 ##################
 #  Get vim Plug  #
 ##################
- curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [[ ${CURL} -eq 0 ]]; then
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+elif
+  echo "Curl not found"
+fi
+
 
 
